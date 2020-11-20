b12bfe82c799fe3d7fd3918b9f082cd40da2ada2
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-11-20 12:26:22 +0000

    Adds some transaction items

--- src/index.js
+++ src/index.js
@@ -13,6 +13,81 @@ app.use((error, _, res, next) => {
   next();
 });
 
+let transactions = [
+  {
+    "id": 1,
+    "amount": 100,
+    "merchant": "Tescos Ltd",
+    "category": "food",
+    "paymentDate": "2019-01-27T14:24:48.960Z"
+  },
+  {
+    "id": 2,
+    "amount": 20,
+    "merchant": "TFL London",
+    "category": "transport",
+    "paymentDate": "2019-02-27T14:24:48.960Z"
+  }, 
+  { 
+    "id": 3,
+    "amount": 32,
+    "merchant": "Bills",
+    "category": "restaurant",
+    "paymentDate": "2019-02-28T14:24:48.960Z"
+  },
+  {
+    "id": 4,
+    "amount": 20,
+    "merchant":  "The Gym",
+    "category": "gym membership",
+    "paymentDate": "2019-02-31T17:24:48.960Z"
+  },
+  {
+    "id": 5,
+    "amount": 50,
+    "merchant": "Asda",
+    "category": "food",
+    "paymentDate": "2019-03-01T11:24:48.960Z"
+  },
+  {
+    "id": 6,
+    "amount": 10,
+    "merchant": "TFL",
+    "cateogy": "transport",
+    "paymentDate": "2019-03-11T09:24:48.960Z"
+  },
+  {
+    "id": 7,
+    "amount": 5,
+    "merchant": "Mcdonalds",
+    "category": "restaurant",
+    "paymentDate": "2019-03-20T18:24:48.960Z"
+  },
+  {
+    "id": 8,
+    "amount": 20,
+    "merchant": "The Gym",
+    "category": "Gym membership",
+    "paymentDate": "2019-03-31T18:24:48.960Z"
+  },
+  {
+    "id": 9,
+    "amount": 30,
+    "merchant": "Tesco",
+    "category": "food",
+    "paymentDate": "2019-04-08T18:24:48.960Z"
+
+  },
+  {
+    "id": 10,
+    "amount": 20,
+    "merchant": "National Express",
+    "category": "travel",
+    "paymentDate": "2019-04-15T10:24:48.960Z"
+
+  }
+]
+
 // routes
 
 
---
+++ user_stories.md
@@ -0,0 +1,12 @@
+```
+As a User
+So that I can gain an understanding of my finances
+I want to see an aggregated list of my transactions by category
+
+```
+
+```
+As a User
+So that I can gain an understanding of if i will run out of money
+I want to see a breakdown of my spending by month
+```
